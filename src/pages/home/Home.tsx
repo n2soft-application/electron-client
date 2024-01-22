c2a485d40f4eed30c33297129971286e838adcae
bonjin <bonjin.app@gmail.com>
2024-01-22 16:18:43 +0900

    feat: 새창 열기 버튼 추가

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -3,9 +3,17 @@ function Home() {
     <div>
       Home
       <div>
-        <button onClick={() => {
-            window.open('/')
-        }}>새창열기</button>
+        <button
+          onClick={() => {
+            window.open("/");
+          }}
+        >
+          새창열기1
+        </button>
+
+        <a href="/" target="_blank">
+          새창열기2
+        </a>
       </div>
     </div>
   );
