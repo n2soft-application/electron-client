fdc96abc080225ef06c27ff8ea9812c61f084cf0
ye1n <uyeinyein@gmail.com>
2024-02-15 17:40:17 +0900

    feat: TestPage1~4 페이지 추가

---
+++ src/pages/test/TestPage2.tsx
@@ -0,0 +1,13 @@
+import Card from "../../components/card";
+
+function TestPage2() {
+  return (
+    <div>
+      <Card extra="p-6 rounded-[20px] mt-3">
+        <p>TestPage2</p>
+      </Card>
+    </div>
+  );
+}
+
+export default TestPage2;
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,16 +1,20 @@
 import { Route, Routes } from "react-router-dom";
 import Home from "../pages/home/Home";
 import Layout from "../components/layout";
+import TestPage1 from "../pages/test/TestPage1";
+import TestPage2 from "../pages/test/TestPage2";
+import TestPage3 from "../pages/test/TestPage3";
+import TestPage4 from "../pages/test/TestPage4";
 
 const AppRouter = () => {
   return (
     <Routes>
       <Route element={<Layout />}>
         <Route path="/" element={<Home />} />
-        <Route path="/admin1" element={<Home />} />
-        <Route path="/admin2" element={<Home />} />
-        <Route path="/admin3" element={<Home />} />
-        <Route path="/admin4" element={<Home />} />
+        <Route path="/admin1" element={<TestPage1 />} />
+        <Route path="/admin2" element={<TestPage2 />} />
+        <Route path="/admin3" element={<TestPage3 />} />
+        <Route path="/admin4" element={<TestPage4 />} />
         <Route path="*" element={<>404</>} />
       </Route>
     </Routes>
