c2393db3c4b7a900f47f3cdf5b4265b1b36aa2ae
ye1n <uyeinyein@gmail.com>
2024-02-16 13:23:50 +0900

    feat: TestPage1 ComplexTable 및 공통 컴포넌트 일부 추가

--- src/pages/test/TestPage1.tsx
+++ src/pages/test/TestPage1.tsx
@@ -1,11 +1,42 @@
-import Card from "../../components/card";
+import ComplexTable from "./components/ComplexTable";
 
 function TestPage1() {
+  const tableDataComplex = [
+    {
+      name: ["ADC", true],
+      progress: 75.5,
+      status: "Approved",
+      date: "2024-01-23",
+    },
+    {
+      name: ["ADD", true],
+      progress: 25.5,
+      status: "Disable",
+      date: "2024-01-23",
+    },
+    {
+      name: ["ADE", true],
+      progress: 90,
+      status: "Error",
+      date: "2024-02-23",
+    },
+    {
+      name: ["ADF", true],
+      progress: 10,
+      status: "Approved",
+      date: "2024-01-11",
+    },
+    {
+      name: ["ADI", true],
+      progress: 50.5,
+      status: "Approved",
+      date: "2024-01-23",
+    },
+  ];
+
   return (
-    <div>
-      <Card extra="p-6 rounded-[20px] mt-3">
-        <p>TestPage1</p>
-      </Card>
+    <div className="mt-3">
+      <ComplexTable tableData={tableDataComplex} />
     </div>
   );
 }
