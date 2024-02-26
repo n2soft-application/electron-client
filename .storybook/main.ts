bea9b3ad868b2246ff0f6116d0d2e44c618b93ee
ye1n <uyeinyein@gmail.com>
2024-02-26 17:40:14 +0900

    feat: Storybook main/preview 설정 수정

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -19,6 +19,7 @@ const config: StorybookConfig = {
   },
   docs: {
     autodocs: "tag",
+    defaultName: "Docs",
   },
   staticDirs: ["../public"],
 };
--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,4 +1,5 @@
 import type { Preview } from "@storybook/react";
+import "../src/index.css";
 
 const preview: Preview = {
   parameters: {
@@ -7,6 +8,27 @@ const preview: Preview = {
       matchers: {
         color: /(background|color)$/i,
         date: /Date$/i,
+      },
+    },
+    docs: {
+      toc: false, // 목차 여부
+      controls: {
+        sort: "requiredFirst", // 정렬
+      },
+    },
+    options: {
+      storySort: {
+        method: "configure",
+        includeNames: true,
+        order: [
+          "Components",
+          ["Button", "Checkbox"],
+          "Layout",
+          ["Sidebar", "Header"],
+          "Example",
+          ["Button", "Page", "Header"],
+          "*",
+        ],
       },
     },
   },
