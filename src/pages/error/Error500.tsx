e1a7d0390f10124b4e465f34a048f9e238f6aef4
bonjin <bonjin.app@gmail.com>
2024-02-26 13:41:04 +0900

    feat: update router, add error page

---
+++ src/pages/error/Error500.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Error500() {
+  return (
+    <div className='h-screen flex justify-center items-center'>
+      Error500
+    </div>
+  )
+}
+
+export default Error500
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,12 +1,11 @@
-import { Route, Routes } from "react-router-dom";
-import Home from "../pages/home/Home";
+import { lazy } from "react";
+import { Navigate, Route, Routes } from "react-router-dom";
 import Layout from "../components/layout";
-import TestPage1 from "../pages/test/TestPage1";
-import TestPage2 from "../pages/test/TestPage2";
-import TestPage3 from "../pages/test/TestPage3";
-import TestPage4 from "../pages/test/TestPage4";
-import SignIn from "../pages/auth/SignIn";
 import PrivateRoute from "./PrivateRoute";
+
+// Pages
+const Home = lazy(() => import("../pages/home/Home"));
+const SignIn = lazy(() => import("../pages/auth/SignIn"));
 
 const AppRouter = () => {
   return (
@@ -14,13 +13,10 @@ const AppRouter = () => {
       <Route element={<Layout />}>
         <Route element={<PrivateRoute />}>
           <Route path="/dashboard" element={<Home />} />
-          <Route path="/admin1" element={<TestPage1 />} />
-          <Route path="/admin2" element={<TestPage2 />} />
-          <Route path="/admin3" element={<TestPage3 />} />
-          <Route path="/admin4" element={<TestPage4 />} />
         </Route>
       </Route>
-      <Route path="*" element={<>404</>} />
+      <Route path="/404" element={<>404</>} />
+      <Route path="*" element={<Navigate to="/404" />} />
       <Route path="/" element={<SignIn />} />
     </Routes>
   );
