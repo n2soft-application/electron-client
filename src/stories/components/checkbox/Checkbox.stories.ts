f27dfe8724a041d2f5d9a949224a742c2369df7a
ye1n <uyeinyein@gmail.com>
2024-02-26 17:33:51 +0900

    feat: Storybook checkbox 작성

---
+++ src/stories/components/checkbox/Checkbox.stories.ts
@@ -0,0 +1,39 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import Checkbox from "../../../components/checkbox/Checkbox";
+
+const meta = {
+  title: "componets/Checkbox",
+  component: Checkbox,
+  parameters: {
+    layout: "centered",
+    componentSubtitle: "체크박스",
+    docs: {
+      description: {
+        component: "추가 설명",
+      },
+    },
+  },
+  tags: ["autodocs"],
+  argTypes: {
+    color: {
+      description: "배경색",
+    },
+    extra: {
+      description: "추가 스타일",
+    },
+  },
+} satisfies Meta<typeof Checkbox>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Checked: Story = {
+  args: {},
+  parameters: {
+    docs: {
+      description: {
+        story: "기본 체크박스입니다",
+      },
+    },
+  },
+};
