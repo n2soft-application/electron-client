a1f5cbd0048da58f36f5c4c7fa9f44e097154fd6
bonjin <bonjin.app@gmail.com>
2024-02-27 17:09:32 +0900

    refactor: scss 삭제

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import { HashRouter } from "react-router-dom";
 import { RecoilRoot } from "recoil";
 import "simplebar-react/dist/simplebar.min.css";
-import "../src/assets/scss/app.scss";
 import AppRouter from "./routes";
 
 function App() {
