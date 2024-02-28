348f42e5d0d96e2d3aaae0b70e11d39924917e35
ye1n <uyeinyein@gmail.com>
2024-02-28 09:58:03 +0900

    feat: Checkbox before style 추가

--- src/components/checkbox/Checkbox.tsx
+++ src/components/checkbox/Checkbox.tsx
@@ -21,7 +21,8 @@ const Checkbox = (props: {
   return (
     <input
       type="checkbox"
-      className={`defaultCheckbox relative flex h-[20px] min-h-[20px] w-[20px] min-w-[20px] appearance-none items-center 
+      className={`before:content-[url('../src/assets/svg/checked.svg')] before:text-white before:opacity-0 before:h-4 before:w-4 
+	  before:absolute before:left-1/2 before:-translate-x-1/2 before:checked:opacity-100 relative flex h-[20px] min-h-[20px] w-[20px] min-w-[20px] appearance-none items-center 
       justify-center rounded-md border border-gray-300 text-white/0 outline-none transition duration-[0.2s]
       checked:border-none checked:text-white hover:cursor-pointer dark:border-white/10 ${
         color === "red"
