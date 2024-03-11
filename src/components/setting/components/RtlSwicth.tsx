65b3955ee19f91a791f1c363ba604721d6ea03da
bonjin <bonjin.app@gmail.com>
2024-03-11 17:34:34 +0900

    feat: settings

--- src/components/setting/components/RtlSwicth.tsx
+++ src/components/setting/components/RtlSwicth.tsx
@@ -5,7 +5,7 @@ function RtlSwicth() {
   const [isRtl, setRtl] = useRtl();
   return (
     <div className="flex justify-between">
-      <div className="text-base text-slate-600 dark:text-slate-300">왼쪽에서 오른쪽으로</div>
+      <div className="text-base text-slate-600 dark:text-slate-300">좌우반전</div>
       <Switch
         value={isRtl}
         onChange={() => setRtl(!isRtl)}
