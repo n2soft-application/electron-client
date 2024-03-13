923ac9e740f85cf1eb14051b3b9f515d3e32e28b
bonjin <bonjin.app@gmail.com>
2024-03-13 16:57:12 +0900

    feat: add basic

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -3,8 +3,8 @@ import { Navigate, Route, Routes } from "react-router-dom";
 import AuthLayout from "../components/layout/AuthLayout";
 import Layout from "../components/layout/Layout";
 import Loading from "../components/loading/Loading";
-import BasicTablePage from "../pages/table/table-basic";
 import BankingPage from "../pages/example/dashboard/banking";
+import BasicTablePage from "../pages/table/table-basic";
 
 // Pages
 const Dashboard = lazy(() => import("../pages/dashboard/Dashboard"));
@@ -20,6 +20,7 @@ const IconsPage = lazy(() => import("../pages/example/icon/IconsPage"));
 const ChatPage = lazy(() => import("../pages/example/chat/ChatPage"));
 const ChangeLog = lazy(() => import("../pages/example/changelog/ChangeLog"));
 const Login = lazy(() => import("../pages/example/login/Login"));
+const BasicWidget = lazy(() => import("../pages/example/widget/BasicWidget"));
 const AppexChartPage = lazy(
   () => import("../pages/example/chart/appex-chart/AppexChartPage")
 );
@@ -41,6 +42,7 @@ const AppRouter = () => {
         <Route path="appex-chart" element={<AppexChartPage />} />
         <Route path="changelog" element={<ChangeLog />} />
         <Route path="table-basic" element={<BasicTablePage />} />
+        <Route path="basic" element={<BasicWidget />} />
 
         <Route path="*" element={<Navigate replace={true} to="/404" />} />
       </Route>
