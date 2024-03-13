fea447ad615ccd2812312b4f013b5ce2530ec24f
bonjin <bonjin.app@gmail.com>
2024-03-13 16:08:35 +0900

    feat: 로고 수정

--- src/components/layout/components/Logo.tsx
+++ src/components/layout/components/Logo.tsx
@@ -4,8 +4,10 @@ import useWidth from "../../../hooks/layout/useWidth";
 
 import MobileLogoWhite from "../../../assets/images/logo/logo-white.png";
 import LogoWhite from "../../../assets/images/logo/logo-white.svg";
-import MobileLogo from "../../../assets/images/logo/logo.png";
-import MainLogo from "../../../assets/images/logo/logo.svg";
+import {
+  default as MainLogo,
+  default as MobileLogo,
+} from "../../../assets/images/logo/logo.png";
 
 function Logo() {
   const [isDark] = useDarkMode();
@@ -14,9 +16,17 @@ function Logo() {
     <div>
       <Link to="/dashboard">
         {width >= breakpoints.xl ? (
-          <img  className="border rounded shadow" src={isDark ? LogoWhite : MainLogo} alt="" />
+          <img
+            className="border rounded shadow"
+            src={isDark ? LogoWhite : MainLogo}
+            alt=""
+          />
         ) : (
-          <img className="border rounded shadow" src={isDark ? MobileLogoWhite : MobileLogo} alt="" />
+          <img
+            className="border rounded shadow"
+            src={isDark ? MobileLogoWhite : MobileLogo}
+            alt=""
+          />
         )}
       </Link>
     </div>
