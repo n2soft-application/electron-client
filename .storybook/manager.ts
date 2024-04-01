9594e5f94f185fda4c82828c292f8ac3f62f429a
ye1n <uyeinyein@gmail.com>
2024-04-01 10:01:47 +0900

    refactor: Storybook Example 스토리 숨김 처리

--- .storybook/manager.ts
+++ .storybook/manager.ts
@@ -4,3 +4,13 @@ import theme from "./theme";
 addons.setConfig({
   theme: theme,
 });
+
+addons.setConfig({
+  sidebar: {
+    filters: {
+      patterns: (item: any) => {
+        return !item.tags.includes("hideInSidebar");
+      },
+    },
+  },
+});
