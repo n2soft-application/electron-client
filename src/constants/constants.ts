7dc1514e19f7d72d58caae21173c12f5059fafe8
bonjin <bonjin.app@gmail.com>
2024-04-19 11:32:52 +0900

    feat: add constants

--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -1,3 +1,9 @@
-export const Constants = {
+export const constants = {
   title: "리테일금융시스템",
+};
+
+export const storageKey = {
+  user: "user",
+  accessToken: "accessToken",
+  refreshToken: "refreshToken",
 };
