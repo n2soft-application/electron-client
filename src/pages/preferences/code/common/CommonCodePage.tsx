db2c6357dfdb5708ae1690998d297269650c5215
ye1n <uyeinyein@gmail.com>
2024-04-22 17:06:36 +0900

    refactor: CommonCodePage 테이블 스크롤시 헤더 상단 고정

--- src/pages/preferences/code/common/CommonCodePage.tsx
+++ src/pages/preferences/code/common/CommonCodePage.tsx
@@ -105,20 +105,22 @@ function CommonCodePage() {
   return (
     <div className="grid grid-cols-1 gap-5 xl:grid-cols-2">
       <Card title="대분류 코드" noborder>
-        <div className="-mx-6 overflow-auto max-h-96">
+        <div className="-mx-6">
           <div className="inline-block min-w-full align-middle">
-            <div className="overflow-hidden ">
-              <table className="min-w-full divide-y table-fixed divide-slate-100 dark:divide-slate-700 whitespace-nowrap">
-                <thead className="bg-slate-200 dark:bg-slate-700">
+            <div className="overflow-x-auto">
+              <table className="min-w-full divide-y divide-slate-100 dark:divide-slate-700">
+                <thead className="block bg-slate-200 dark:bg-slate-700">
                   <tr>
-                    {firstcolumns.map((column, i) => (
-                      <th key={i} scope="col" className="text-center table-th">
-                        {column.label}
-                      </th>
-                    ))}
+                    <th className="text-center w-11 table-th">순번</th>
+                    <th className="text-center w-14 table-th">코드</th>
+                    <th className="text-center w-72 table-th">코드명</th>
+                    <th className="w-16 text-center table-th">사용여부</th>
+                    <th className="w-16 text-center table-th">주요코드</th>
+                    <th className="text-center w-14 table-th">코드셋</th>
+                    <th className="text-center w-28 table-th">생성일</th>
                   </tr>
                 </thead>
-                <tbody className="bg-white divide-y divide-slate-100 dark:bg-slate-800 dark:divide-slate-700">
+                <tbody className="block overflow-auto bg-white divide-y max-h-96 divide-slate-100 dark:bg-slate-800 dark:divide-slate-700">
                   {codeGroup1.map((code, i) => (
                     <tr
                       key={i}
@@ -129,15 +131,15 @@ function CommonCodePage() {
                       }`}
                       onClick={() => setSelected(code.kind)}
                     >
-                      <td className="table-td">{i + 1}</td>
-                      <td className="table-td">{code.kind}</td>
-                      <td className="text-left table-td">{code.name}</td>
-                      <td className="table-td">{code.useYn}</td>
-                      <td className="table-td">-</td>
-                      <td className="table-td">
+                      <td className="w-11 table-td">{i + 1}</td>
+                      <td className="w-14 table-td">{code.kind}</td>
+                      <td className="text-left w-72 table-td">{code.name}</td>
+                      <td className="w-16 table-td">{code.useYn}</td>
+                      <td className="w-16 table-td">-</td>
+                      <td className="w-14 table-td">-</td>
+                      <td className="w-28 table-td">
                         {dayjs(code.createdDate).format("YYYY-MM-DD")}
                       </td>
-                      <td className="table-td">-</td>
                     </tr>
                   ))}
                 </tbody>
@@ -147,34 +149,36 @@ function CommonCodePage() {
         </div>
       </Card>
       <Card title="소분류 코드" noborder>
-        <div className="-mx-6 overflow-auto max-h-96">
+        <div className="-mx-6">
           <div className="inline-block min-w-full align-middle">
             <div className="overflow-hidden ">
-              <table className="min-w-full divide-y table-fixed divide-slate-100 dark:divide-slate-700 whitespace-nowrap">
-                <thead className="bg-slate-200 dark:bg-slate-700">
+              <table className="min-w-full divide-y table-fixed divide-slate-100 dark:divide-slate-700">
+                <thead className="block bg-slate-200 dark:bg-slate-700">
                   <tr>
-                    {secondcolumns.map((column, i) => (
-                      <th key={i} scope="col" className="text-center table-th">
-                        {column.label}
-                      </th>
-                    ))}
+                    <th className="text-center w-11 table-th">순번</th>
+                    <th className="text-center w-14 table-th">코드</th>
+                    <th className="text-center w-72 table-th">코드명</th>
+                    <th className="w-16 text-center table-th">보기순서</th>
+                    <th className="w-16 text-center table-th">사용여부</th>
+                    <th className="text-center w-28 table-th">등록일</th>
+                    <th className="text-center w-28 table-th">변경일</th>
                   </tr>
                 </thead>
-                <tbody className="bg-white divide-y divide-slate-100 dark:bg-slate-800 dark:divide-slate-700">
+                <tbody className="block overflow-auto bg-white divide-y max-h-96 divide-slate-100 dark:bg-slate-800 dark:divide-slate-700">
                   {codeGroup2.map((code, i) => (
                     <tr
                       key={i}
                       className="text-center hover:bg-slate-100 dark:hover:bg-slate-700"
                     >
-                      <td className="table-td">{i + 1}</td>
-                      <td className="table-td">-</td>
-                      <td className="text-left table-td">{code.name}</td>
-                      <td className="table-td">{code.priority}</td>
-                      <td className="table-td">{code.useYn}</td>
-                      <td className="table-td">
+                      <td className="w-11 table-td">{i + 1}</td>
+                      <td className="w-14 table-td">-</td>
+                      <td className="text-left w-72 table-td">{code.name}</td>
+                      <td className="w-16 table-td">{code.priority}</td>
+                      <td className="w-16 table-td">{code.useYn}</td>
+                      <td className="w-28 table-td">
                         {dayjs(code.createdDate).format("YYYY-MM-DD")}
                       </td>
-                      <td className="table-td">
+                      <td className="w-28 table-td">
                         {dayjs(code.createdDate).format("YYYY-MM-DD")}
                       </td>
                     </tr>
