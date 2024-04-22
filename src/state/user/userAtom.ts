56baffc77c4a18d2637258b006370555f1a53f9f
bonjin <bonjin.app@gmail.com>
2024-04-22 11:43:05 +0900

    fix: log 삭제

--- src/state/user/userAtom.ts
+++ src/state/user/userAtom.ts
@@ -2,8 +2,6 @@ import { atom, selector } from "recoil";
 
 type UserType = {};
 
-console.log('localStorage.getItem("user")', localStorage.getItem("user"))
-
 export const userState = atom<UserType | null>({
   key: "userState",
   default: localStorage.getItem("user") && JSON.parse(localStorage.getItem("user")!),
